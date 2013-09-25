d95046c1f3258e8036a49a373cc9cf33d6ee6bec
Matt Young <mpyoung@gmail.com>
2013-09-25 19:40:12 +0100

    Added first attempt at finger board rendering

--- ux/chord.ux/controllers/chord.js
+++ ux/chord.ux/controllers/chord.js
@@ -10,6 +10,7 @@ function ChordCtrl($scope,chordFactory) {
         chordFactory.getChord(chordName, function(data){
                 $scope.chordName = data.Name;
                 $scope.fingerings = data.Fingerings;
+                $scope.frets = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
             }
         );
     }
